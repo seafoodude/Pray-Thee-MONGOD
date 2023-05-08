1cb4858eb8ca2c1257ba8f5062fe1e9553cedc6d
seafoodude <michaelhuang777@gmail.com>
2023-05-08 16:45:05 -0700

    changed up connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkDB', {
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
